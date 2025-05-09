bb8000b9a258ae44a8656c6d4f0b299266aa045a
Searidang Pa <dangsyncpa@gmail.com>
2025-05-09 13:27:05 -0400

    Move get_os_mode and save_theme_preference into setup to ensure they are available when setting theme.

--- lua/theme-loader.lua
+++ lua/theme-loader.lua
@@ -13,27 +13,6 @@ M.config = {
   },
 }
 
-local function get_os_mode()
-  local is_light = true
-  if vim.fn.has 'win32' == 1 then
-    local result = vim.fn.system 'reg query "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize" /v AppsUseLightTheme'
-    is_light = not result:match '0x0'
-  else
-    local result = vim.fn.system 'defaults read -g AppleInterfaceStyle 2>/dev/null || echo "Light"'
-    is_light = not result:match 'Dark'
-  end
-  return is_light
-end
-
-local function save_theme_preference(opts)
-  local is_light_mode = opts.is_light_mode or false
-  local file = io.open(M.cache_file, 'w')
-  if file then
-    file:write(is_light_mode and 'light' or 'dark')
-    file:close()
-  end
-end
-
 local function load_prev_theme_preference()
   local file = io.open(M.cache_file, 'r')
   if file then
@@ -69,18 +48,7 @@ local function set_theme(opts)
     vim.cmd.colorscheme(theme_config.colorscheme)
     lualine_refresh(mode)
   end
-
-  save_theme_preference { is_light_mode = is_light_mode }
 end
-
-function M.toggle_theme()
-  local current_theme = load_prev_theme_preference()
-  set_theme { is_light_mode = not current_theme }
-end
-
-function M.set_light_theme() set_theme { is_light_mode = true } end
-
-function M.set_dark_theme() set_theme { is_light_mode = false } end
 
 function M.setup(opts)
   M.config = vim.tbl_deep_extend('force', M.config, opts or {})
@@ -90,8 +58,30 @@ function M.setup(opts)
   end
 
   vim.schedule(function()
+    local function get_os_mode()
+      local is_light = true
+      if vim.fn.has 'win32' == 1 then
+        local result = vim.fn.system 'reg query "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize" /v AppsUseLightTheme'
+        is_light = not result:match '0x0'
+      else
+        local result = vim.fn.system 'defaults read -g AppleInterfaceStyle 2>/dev/null || echo "Light"'
+        is_light = not result:match 'Dark'
+      end
+      return is_light
+    end
+
+    local function save_theme_preference(opts)
+      local is_light_mode = opts.is_light_mode or false
+      local file = io.open(M.cache_file, 'w')
+      if file then
+        file:write(is_light_mode and 'light' or 'dark')
+        file:close()
+      end
+    end
+
     local is_light_mode_from_os = get_os_mode()
     set_theme { is_light_mode = is_light_mode_from_os }
+    save_theme_preference { is_light_mode = is_light_mode_from_os }
   end)
 end
 
