1f28d1d0c92f26b4710f33190a4080452edbfa9b
Searidang Pa <dangsyncpa@gmail.com>
2025-05-12 13:01:59 -0400

    Refactor set_os_theme to set_theme_based_on_os, add toggle_os_theme function to switch OS theme and update theme accordingly.

--- lua/theme-loader.lua
+++ lua/theme-loader.lua
@@ -44,10 +44,34 @@ function M.set_theme(is_light_mode)
   end, 1000)
 end
 
-function M.set_os_theme()
+function M.set_theme_based_on_os()
   local is_light_mode = get_os_theme()
   M.set_theme(is_light_mode)
   M.save_theme_preference(is_light_mode)
+end
+
+function M.toggle_os_theme()
+  if vim.fn.has 'win32' == 1 then
+    local is_light = get_os_theme()
+    local new_value = is_light and '0' or '1'
+    vim.fn.system(
+      'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize" /v AppsUseLightTheme /t REG_DWORD /d ' .. new_value .. ' /f'
+    )
+  else
+    local script = [[
+    osascript -e '
+      tell application "System Events"
+        tell appearance preferences
+          set dark mode to not dark mode
+        end tell
+      end tell
+    '
+    ]]
+    vim.fn.system(script)
+  end
+  local new_theme = get_os_theme() and 'Light' or 'Dark'
+  M.set_theme(new_theme)
+  vim.notify('OS Theme toggled to: ' .. new_theme)
 end
 
 function M.setup()
@@ -55,7 +79,7 @@ function M.setup()
     M.set_theme(load_theme_preference())
   end
 
-  vim.defer_fn(M.set_os_theme, 1000)
+  vim.defer_fn(M.set_theme_based_on_os, 1000)
 end
 
 return M
